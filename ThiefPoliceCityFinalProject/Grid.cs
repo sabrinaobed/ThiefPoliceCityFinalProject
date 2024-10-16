cd4371abdb9a0ddbcc494763b559d0038e005b62
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2024-10-16 10:37:06 +0200

    Decalred fields of grid class

--- ThiefPoliceCityFinalProject/Grid.cs
+++ ThiefPoliceCityFinalProject/Grid.cs
@@ -8,5 +8,10 @@ namespace ThiefPoliceCityFinalProject
 {
     internal class Grid
     {
+        //Fields
+        //Decalring fields with private access modifier so they are not accessed outside the grid class 
+        private int width;    //col
+        private int height;   //rows
+        private char[,] grid; //2D char array represnting actual grid
     }
 }
