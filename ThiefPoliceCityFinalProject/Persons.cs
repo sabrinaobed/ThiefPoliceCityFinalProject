c3d41aecdad4b9448f9e49d7a229f8dbe267b61a
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2024-10-16 14:16:25 +0200

    Created properties and a field/object of Random type

--- ThiefPoliceCityFinalProject/Persons.cs
+++ ThiefPoliceCityFinalProject/Persons.cs
@@ -8,5 +8,15 @@ namespace ThiefPoliceCityFinalProject
 {
     internal class Persons
     {
-    }
+
+        //Properties
+        public int XCoordinate{ get; set; } //col
+        public int YCoordinate { get; set; } //row
+
+        public virtual char PersonRepresentation { get; } //characters for all persons,its virtual so the subclasses can override it.
+
+        protected static Random random = new Random(); // field of type Random and also an object created by new random(),its protected so its only accessed by base class and its subclasses.
+
+
+    } 
 }
