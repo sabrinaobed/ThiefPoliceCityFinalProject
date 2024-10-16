9f0a9ba14ebaecf6e146dbf4bbd02b562f5c2af6
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2024-10-16 10:50:07 +0200

    Created a parameterized constructor of class Grid

--- ThiefPoliceCityFinalProject/Grid.cs
+++ ThiefPoliceCityFinalProject/Grid.cs
@@ -12,6 +12,14 @@ namespace ThiefPoliceCityFinalProject
         //Decalring fields with private access modifier so they are not accessed outside the grid class 
         private int width;    //col
         private int height;   //rows
-        private char[,] grid; //2D char array represnting actual grid
+        private char[,] grid; //2D char array representing actual grid
+
+        //Parameterized Constructor Method
+        public Grid(int width, int height)
+        {
+            this.width = width; //this.width refers to the  private field of the class and width refers to parameter width value passed to constructor
+            this.height = height;
+            grid = new char[width, height]; //creating a new object & represents different elements of grid like empty spaces boundaries etc
+        }
     }
 }
